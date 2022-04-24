76e35e401d12736d6ae5f9174f047a606f9f5111
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2022-04-24 17:42:15 +0300

    add

--- 000-task-manager/db/connect.js
+++ 000-task-manager/db/connect.js
@@ -13,4 +13,15 @@ const connectDB = (url) => {
 //         .then(() => console.log('Connected to the db...'))
 //         .catch((err) => console.log(err));
 
+/*
+const connectDB = (url) => {
+     return mongoose.connect(url,{
+             useNewUrlParser: true,
+             useCreateIndex: true,
+             useFindAndModify: false,
+             useUnifiedTopology: true,
+     })
+}
+*/
+
 module.exports = connectDB;
