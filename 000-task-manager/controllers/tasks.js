533710d7f77a7ef82cd59bf5bca571126a637328
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2022-04-27 17:38:44 +0300

    add

--- 000-task-manager/controllers/tasks.js
+++ 000-task-manager/controllers/tasks.js
@@ -39,8 +39,19 @@ const updateTask = (req,res) => {
     res.send("update task");
 }
 
-const deleteTask = (req,res) => {
-    res.send("delete task");
+const deleteTask = async (req,res) => {
+    try {
+        //res.send("delete task");
+        const {id:taskID} = req.params;
+        const task = await Task.findOneAndDelete({_id:taskID});
+        if(!task){
+            return res.status(404).json({msg:`No Task with id: ${taskID}`});
+           // res.status(200).json({task:null, status: 'delete it'}); //another way
+        }
+        res.status(200).json({task});
+    } catch (error) {
+        res.status(500).json({msg:error});
+    }
 }
 
 module.exports = {
