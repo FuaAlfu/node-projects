48542dceb82f1dda71e581ca2209f1973f7abe4d
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2022-05-31 16:05:28 +0300

    add

--- 003-store-api/controller/controller.js
+++ 003-store-api/controller/controller.js
@@ -1,20 +1,32 @@
 const Product = require('../models/product');
 
-const getAllProductsStatics = (req,res) => {
+const getAllProductsStatics = async (req,res) => {
    // throw new Error('Not Found --> testing aysinc error'); //for test
    //res.status(200).json({msg:'Products testing route'});
-   const products = await Product.find({
+  
+   const products = await Product.find({ //await
        name: 'vase table',
    });
     res.status(200).json({products,nbHits: products.length});
 }
 
-const getAllProducts = (req,res) => {
+const getAllProducts = async (req,res) => {
    // console.log('====================================');
    // console.log(req.query);
    // console.log('====================================');
   // res.status(200).json({msg:'Products route'});
-   const products = await Product.find(req.query);
+  const {featured,company} = req.query
+  const queryObject = {}
+
+  if(featured){
+      queryObject.featured = featured === 'true' ? true : false;
+  }
+  if(company){
+      queryObject.company = company;
+  }
+  //const products = await Product.find(req.query);
+  console.log(queryObject);
+  const products = await Product.find(queryObject); //await
     res.status(200).json({products,nbHits: products.length});
 }
 
